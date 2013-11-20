dc5517ecf8f796e24c284e23e42baa641fcfaa09
DELES <deles.damien@gmail.com>
2013-11-20 15:09:32 +0100

    Commit Initial

---
+++ iTaskFamily/DDConstant.h
@@ -0,0 +1,13 @@
+//
+//  DDConstant.h
+//  iTaskFamily
+//
+//  Created by Damien DELES on 20/11/2013.
+//  Copyright (c) 2013 Damien DELES. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDConstant : NSObject
+
+@end
---
+++ iTaskFamily/DDConstant.m
@@ -0,0 +1,13 @@
+//
+//  DDConstant.m
+//  iTaskFamily
+//
+//  Created by Damien DELES on 20/11/2013.
+//  Copyright (c) 2013 Damien DELES. All rights reserved.
+//
+
+#import "DDConstant.h"
+
+@implementation DDConstant
+
+@end
